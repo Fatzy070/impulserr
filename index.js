cf55d81f6d2298e1d36a17fc8a19d2eae2153370
Faruk Ogunsola <ogunsolafaruk8@example.com>
2025-05-18 13:49:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function toggleText() {
+    const text = document.getElementById("hiddenText");
+    const icon = document.getElementById("menuIcon")
+
+    text.classList.toggle("hidden");
+    if (text.classList.contains("hidden")) {
+        icon.classList.remove("fa-xmark");
+        icon.classList.add("fa-bars" , 'text-white');
+      } else {
+        icon.classList.remove("fa-bars" , 'text-white');
+        icon.classList.add("fa-xmark" , 'text-[rgba(133,72,42,255)]' ,);
+      }
+}
